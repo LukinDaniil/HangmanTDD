e7be37c5994677264a8e04c69d214bcf98ee7b64
Даниил Лукин <lukindaniil@mail.ru>
2025-04-23 00:44:55 +0700

    Создал тест для проверки установки текущего состояния отгадывания слова при создании игры

--- HangmanTDDnUnitTests/HangmanTests.cs
+++ HangmanTDDnUnitTests/HangmanTests.cs
@@ -16,18 +16,25 @@ namespace HangmanTDDnUnitTests
             Hangman hangman = new Hangman();
             Assert.NotNull(hangman);
         }
+        [Test]
         public void HangManClassGameInitialisationTest()
         {
             Hangman hangman = new Hangman("test");
             Assert.AreEqual("test", hangman.hiddenWord);
         }
+        [Test]
         public void HangManClassSettingOfRemainingAttemptsTest()
         {
             Hangman hangman = new Hangman();
             hangman.setRemainingAttempts(5);
             Assert.AreEqual(5, hangman.remainingAttempts);
         }
-
+        [Test]
+        public void HangManClassCheckCurrentGameStateEqualHiddenWordTest()
+        {
+            Hangman hangman = new Hangman("test");
+            Assert.AreEqual("____", hangman.checkCurrentGameState());
+        }
 
     }
 }
