ec6bcd2e5937c113ef8a6102077853734708cb6b
Даниил Лукин <lukindaniil@mail.ru>
2025-04-22 23:48:55 +0700

    Добавьте файлы проекта.

---
+++ GameHangman/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameHangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
